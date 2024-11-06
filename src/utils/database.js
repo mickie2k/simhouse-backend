5331f73ac84e50f984e55acaa48b63f668ef7514
mickie2k <63552271+mickie2k@users.noreply.github.com>
2024-11-06 21:10:54 +0700

    add allow domain

--- src/utils/database.js
+++ src/utils/database.js
@@ -11,6 +11,7 @@ async function initSQL() {
 			user: process.env.DB_USER,
 			password: process.env.DB_PASS,
 			database: process.env.DB,
+			port: process.env.DB_PORT,
 			waitForConnections: true,
 			connectionLimit: 10, // Adjust the limit as necessary
 			queueLimit: 0,
